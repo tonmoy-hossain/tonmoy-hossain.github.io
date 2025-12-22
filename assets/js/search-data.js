ba71d9a6b137dd34dd02a4d1638c6d7b889e0d28
tonmoy-hossain <59301323+tonmoy-hossain@users.noreply.github.com>
2025-12-22 22:31:34 +0000

    Deploying to gh-pages from @ tonmoy-hossain/tonmoy-hossain.github.io@0c1e56d89e30357a54c76e93afa7f45383e3b885 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
