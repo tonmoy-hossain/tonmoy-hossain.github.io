d0261d717894ce4ea9173bc75d34c136698cc29f
tonmoy-hossain <59301323+tonmoy-hossain@users.noreply.github.com>
2026-07-19 03:09:37 +0000

    Deploying to gh-pages from @ tonmoy-hossain/tonmoy-hossain.github.io@c66556ec21b92b32e8fa9da1457bc9d5be09e86c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/services/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "nav-through-my-lens",
           title: "Through My Lens",
           description: "A visual journey through places and moments",
